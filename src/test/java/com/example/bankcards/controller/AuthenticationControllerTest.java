39119858ba25fb37148489cca1f5265209de0637
grinn <grinnosse@gmail.com>
2025-08-05 13:27:06 +0300

    second version

--- src/test/java/com/example/bankcards/controller/AuthenticationControllerTest.java
+++ src/test/java/com/example/bankcards/controller/AuthenticationControllerTest.java
@@ -3,7 +3,6 @@ package com.example.bankcards.controller;
 import com.example.bankcards.BankCardsApplication;
 import com.example.bankcards.dto.AuthenticationRequestDto;
 import com.example.bankcards.dto.AuthenticationResponseDto;
-import com.example.bankcards.repository.UserRepository;
 import com.example.bankcards.security.JwtService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterAll;
